a22756a95ed1d24d1ea13676836adb0803a97e9d
Noah Broyles <noah@javamate.net>
2021-11-05 16:57:34 -0400

    Ready for testing

--- database.py
+++ database.py
@@ -1,5 +1,15 @@
+import os
 import time
 import pymssql
+
+
+try:
+    DBHOST = os.environ['DBHOST']
+    DBUSER = os.environ['DBUSER']
+    DBPASSWD = os.environ['DBPASSWD']
+    DATABASE = os.environ['DATABASE']
+except KeyError:
+    DBHOST, DBUSER, DBPASSWD, DATABASE = None, None, None, None
 
 
 class Iter(type):
@@ -59,12 +69,23 @@ class Response:
         return str([str(row) for row in self._rows])
 
 
+
+def _chunker(seq: list, size: int):
+    return (seq[pos:pos + size] for pos in range(0, len(seq), size))
+
+
+
 class Database:
 
-    def __init__(self, server: str, database: str, username: str, password: str):
+    def __init__(self, server: str = DBHOST, port: int = 1433, database: str = DATABASE, username: str = DBUSER, password: str = DBPASSWD, charset='UTF-8'):
+        self._connection = pymssql.connect(server=server, user=username, password=password, database=database, charset=charset, port=port)
+        self._cursor = self._connection.cursor(as_dict=True)
 
-        self._conn = pymssql.connect(server, username, password, database)
-        self._cursor = self._conn.cursor(as_dict=True)
+    def __enter__(self):
+        return self
+
+    def __exit__(self, _type, _value, _traceback):
+        self.close()
 
     def query(self, sqlQuery: str, params: list = None):
         """
@@ -83,7 +104,7 @@ for SELECT and EXEC if the stored procedure is meant to return something.
         else:
             new_sql_query = sqlQuery
 
-        cursor = self._conn.cursor(as_dict=True)
+        cursor = self._connection.cursor(as_dict=True)
 
         if params:
             cursor.execute(new_sql_query, tuple(params))
@@ -93,12 +114,44 @@ for SELECT and EXEC if the stored procedure is meant to return something.
         cursor.close()
         return Response(res)
 
-    def execute(self, sqlStmt: str, params: list = None, commit: bool = True, timeout_seconds=100):
+
+    def execute_proc(self, procCode: str, params: list, commit: bool = False, convert_blanks_to_nulls: bool = True, timeout_seconds: int = 200):
+        """
+Run a stored procedure in the database and wait for it to finish
+        :param convert_blanks_to_nulls: Uses NULL as a substitute for blank strings ("") in the parameters
+        :param procCode: The code to run the stored procedure
+        :param params: Any parameters ya might want
+        :param commit: Commits after running if True
+        :param timeout_seconds: The max number of seconds to wait for the stored procedure to finish execution
+        """
+        cursor = self._connection.cursor()
+
+        if params:
+            if convert_blanks_to_nulls:
+                params = [p if p != "" else None for p in params]
+            cursor.execute(procCode, *params)
+        else:
+            cursor.execute(procCode)
+
+        # Wait to finish
+        slept_times = 0
+        while cursor.nextset():
+            if (slept_times / 2) >= timeout_seconds:
+                break
+            time.sleep(0.5)
+            slept_times += 1
+
+        cursor.close()
+        if commit:
+            self._connection.commit()
+
+    def execute_stmt(self, sqlStmt: str, params: list = None, commit: bool = False, convert_blanks_to_nulls: bool = True, timeout_seconds: int = 100):
         """
 Parameterizes statement and runs in the database. Use for INSERT, UPDATE, DROP, and EXEC commands where no results are expected to be returned.
         :param commit: Commits the changes in the database if True
         :param sqlStmt: The code to run
         :param params: Any parameters, substitute for question marks ('?') in the sqlStmt
+        :param timeout_seconds: The max number of seconds to wait for the statement to run
         """
         if params:
             new_sql_stmt = ''
@@ -110,24 +163,85 @@ Parameterizes statement and runs in the database. Use for INSERT, UPDATE, DROP, 
         else:
             new_sql_stmt = sqlStmt
 
-        # with open('C:/bestnest_com/web/3PL/query.sql', 'w') as qf:
-        #     qf.write(newSqlStmt)
-
         if params:
+            if convert_blanks_to_nulls:
+                params = [p if p != "" else None for p in params]
             self._cursor.execute(new_sql_stmt, *params)
         else:
             self._cursor.execute(new_sql_stmt)
 
-        slept_times = 0
-        while self._cursor.nextset():
-            if (slept_times / 10) >= timeout_seconds:
-                break
-            time.sleep(0.1)
-            slept_times += 1
-
         if commit:
-            self._conn.commit()
+            self._connection.commit()
+
+
+    def execute_many(self, sqlStmt: str, multipleParams: list, fast: bool = False, commit: bool = False, convert_blanks_to_nulls: bool = True):
+        cursor = self._connection.cursor()
+        statements = []
+
+        for param_set in multipleParams:
+            statements.append(PreparedStatement(sql=sqlStmt, params=param_set, convert_blanks_to_nulls=convert_blanks_to_nulls).sql)
+
+        if fast:
+            cursor.execute(";".join(statements))
+        else:
+            # If we are not doing it 'fast', we will split it up into batches of commands
+            for command_chunk in _chunker(statements, 100):
+                cursor.execute(';'.join(command_chunk))
+
+        cursor.close()
+        if commit:
+            self._connection.commit()
 
     def close(self):
-        self._conn.commit()
-        self._conn.close()
+        self._connection.commit()
+        self._connection.close()
+
+
+class ParameterMismatchError(Exception):
+    pass
+
+
+class PreparedStatement:
+    """
+    Please please PLEASE only use this class for debugging. It is NOT secure against SQL injections, since it simply substitutes
+    parameter placeholders with whatever value they are assigned, with no data validation.
+    DO NOT USE WITH UN-SANITIZED USER DATA!!!
+    This class is very useful for debugging queries to see how they look with all their parameters in place. For huge SQL statements,
+    performance is much slower than just executing the query with the params using Database.execute_stmt() or Database.query().
+    """
+    def __init__(self, sql: str, params: list, convert_blanks_to_nulls: bool = True):
+        self._sql = sql
+        self._params = params
+        self._blank_conversion = convert_blanks_to_nulls
+        self._finished_sql_statement: str = ''
+        self._prepare()
+
+    def _prepare(self):
+        if not self._params:
+            self._finished_sql_statement = self._sql
+            return
+        try:
+            param_index = 0
+            for char in self._sql:
+                if char == '?':
+                    if type(self._params[param_index]) == str:
+                        if self._params[param_index] == '':
+                            if self._blank_conversion:
+                                self._finished_sql_statement += 'NULL'
+                        else:
+                            self._finished_sql_statement += f"""'{self._params[param_index].replace("'", "''")}'"""
+                    else:
+                        self._finished_sql_statement += str(self._params[param_index])
+                    param_index += 1
+                else:
+                    self._finished_sql_statement += char
+        except IndexError:
+            raise ParameterMismatchError("The number of parameters in the SQL code did not match the params list")
+
+        self.sql = self.get_finished_sql()
+
+    def get_finished_sql(self):
+        return self._finished_sql_statement
+
+    def __str__(self):
+        return self.sql
